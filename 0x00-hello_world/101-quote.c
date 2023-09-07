741a37db620ae5cb6616f8e3e1e87bd4a5f3930d
Mira <benamara97amira@gmail.com>
2023-09-07 12:46:25 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	exit(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
 }
 
