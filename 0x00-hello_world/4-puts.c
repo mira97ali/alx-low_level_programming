9d485cf981a1c16de51bb87b8564c906e716ccad
Mira <benamara97amira@gmail.com>
2023-09-07 09:10:30 -0700

    adjustments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main: Entrypoint
+ * main - Entrypoint
  *
- * args: none
+ * Description: print a sentence
  *
- * returns: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
