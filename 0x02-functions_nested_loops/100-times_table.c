3632ccc329f19b947442fd0b7ed4d8a1c6b39339
Mira <benamara97amira@gmail.com>
2023-09-12 14:28:02 -0700

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,8 @@ void print_times_table(int n)
 					_putchar('0' + result);
 				else
 				{
-					_putchar('0' + result / 10);
+					_putchar('0' + result / 100);
+					_putchar('0' + (result / 10) % 10);
 					_putchar('0' + result % 10);
 				}
 			}
