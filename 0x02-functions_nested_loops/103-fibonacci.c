924b2a384e9548b0aea35bd7de8a0460dca6ece2
Mira <benamara97amira@gmail.com>
2023-09-12 13:27:51 -0700

    fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,18 +4,22 @@
  *
  * Return: Always 0 (Success).
  */
-int main() {
-	long long int a = 1, b = 2, temp;
-	long long int sum = 0;
-	while (b <= 4000000) {
-		if (b % 2 == 0) {
+int main(void)
+{
+	unsigned long int a = 1, b = 2, temp;
+	int sum = 0;
+
+	while (b <= 4000000)
+	{
+		if (b % 2 == 0)
+		{
 			sum += b;
 		}
 		temp = a;
 		a = b;
 		b += temp;
 	}
-	printf("%lu\n", sum);
-	return 0;
+	printf("%d\n", sum);
+	return (0);
 }
 
