9a9b20920d51cd72746e89260beaed966584496d
Mira <benamara97amira@gmail.com>
2023-09-12 14:11:14 -0700

    change code

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,20 +6,40 @@
  */
 int main(void)
 {
-	unsigned long int a = 1, b = 2;
-	unsigned long int temp;
-	int count = 0;
+	unsigned long int i;
+	unsigned long int before = 1;
+	unsigned long int after = 2;
+	unsigned long int end = 1000000000;
+	unsigned long int before1;
+	unsigned long int before2;
+	unsigned long int after1;
+	unsigned long int after2;
 
-	while (count <= 96)
+	printf("%lu", before);
+
+	for (i = 1; i < 91; i++)
 	{
-		printf("%lu, ", a);
-		temp = a;
-		a = b;
-		b = temp + b;
-		count++;
+		printf(", %lu", after);
+		after += before;
+		before = after - before;
 	}
 
-	printf("%lu\n", a);
+	before1 = (before / end);
+	before2 = (before % end);
+	after1 = (after / end);
+	after2 = (after % end);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", after1 + (after2 / end));
+		printf("%lu", after2 % end);
+		after1 = after1 + before1;
+		before1 = after1 - before1;
+		after2 = after2 + before2;
+		before2 = after2 - before2;
+	}
+	printf("\n");
+
 	return (0);
 }
 
