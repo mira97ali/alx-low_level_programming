569eacd4f60aeff40ecbee72ce4106ecc9e3acea
Mira <benamara97amira@gmail.com>
2023-09-13 06:01:46 -0700

    first

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test the positive_or_negative function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i;
+
+    i = 0;
+    positive_or_negative(i);
+
+    return (0);
+}
