eb891b7055efa4421e8684407e3c1113299a4e02
Mira <benamara97amira@gmail.com>
2023-10-19 14:27:16 -0700

    list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,21 +2,20 @@
 #include "lists.h"
 
 /**
- * list_len - Counts the number of elements in a linked list.
- * @h: A pointer to the list_t linked list.
- *
- * Return: The count of elements in the linked list.
- */
-size_t list_len(const list_t *h)
+* custom_list_len - Counts the number of elements in a linked list.
+* @current: A pointer to the list_t linked list.
+*
+* Return: The count of elements in the linked list.
+*/
+size_t custom_list_len(const list_t *current)
 {
-    size_t count = 0;
+	size_t elementCount = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+	while (current != NULL)
+	{
+		elementCount++;
+		current = current->next;
+	}
 
-    return count;
+	return (elementCount);
 }
-
