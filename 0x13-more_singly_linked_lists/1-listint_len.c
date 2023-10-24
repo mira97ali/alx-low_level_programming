28b8c1ad6adcd275e14a94b023f2e6ba35c977ae
Mira <benamara97amira@gmail.com>
2023-10-24 04:20:11 -0700

    second task

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 
 /**
-* print_listint - prints all the elements
-* @head: linked list of type listint_t to be printed
+* listint_len - returns the num of elements
+* @head: linked list of type listint_t to traverse
 *
 * Return: num of nodes
 * Author: Amira
 */
 
-size_t print_listint(const listint_t *head)
+size_t listint_len(const listint_t *head)
 {
 	size_t node_count = 0;
 
 	while (head)
 	{
-		printf("%d\n", head->n);
 		node_count++;
 		head = head->next;
 	}
 
 	return (node_count);
 }
+
