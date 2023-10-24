ec6b57c5f64a64062eaca701e26e75d9e1b71e82
Mira <benamara97amira@gmail.com>
2023-10-24 04:04:35 -0700

    first

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all the elements
+* @head: linked list of type listint_t to be printed
+*
+* Return: num of nodes
+*/
+
+size_t print_listint(const listint_t *head)
+{
+	size_t node_count = 0;
+
+	while (head)
+	{
+		printf("%d\n", head->n);
+		node_count++;
+		head = head->next;
+	}
+
+	return node_count;
+}
