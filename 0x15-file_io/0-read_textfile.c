30fbc8f1e90b15648db9d660e977f6fc3338cdfd
Mira <benamara97amira@gmail.com>
2023-10-30 15:17:27 -0700

    fix betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,33 +11,31 @@
 *
 * Author: Amira
 */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t file_descriptor, bytes_read, bytes_written;
 	char *file_contents;
 
-	if (filename is NULL)
+	if (filename == NULL)
 		return (0);
 
 	file_contents = malloc(sizeof(char) * letters);
-	if (file_contents is NULL)
+	if (file_contents == NULL)
 		return (0);
 
 	file_descriptor = open(filename, O_RDONLY);
 	bytes_read = read(file_descriptor, file_contents, letters);
 	bytes_written = write(STDOUT_FILENO, file_contents, bytes_read);
 
-	if (file_descriptor == -1 || bytes_read == -1 |
-		| bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(file_contents);
-		return (0);
-	}
+	if (file_descriptor == -1 || 
+	bytes_read == -1 || 
+	bytes_written == -1 || 
+	bytes_written != bytes_read)
+{
+	free(file_contents);
+	return (0);
+}
 
 	free(file_contents);
 	close(file_descriptor);
-
-	return (bytes_written);
-}
 
