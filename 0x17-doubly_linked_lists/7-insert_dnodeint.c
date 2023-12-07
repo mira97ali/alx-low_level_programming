18fcc5b83a22d10a1679b0aad548994ca282a0e2
mira97ali <140742998+mira97ali@users.noreply.github.com>
2023-12-07 20:32:08 +0300

    more fixes on betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index - Inserts a new node at
-* a given position.
-*
-* @head: The head of the list.
-* @index: The index of the new node.
-* @n: The value of the new node.
-* Return: The address of the new node, or NULL if it failed.
-* Author: Amira
-*/
+ * insert_dnodeint_at_index - Inserts a new node at
+ * a given position.
+ *
+ * @head: The head of the list.
+ * @index: The index of the new node.
+ * @n: The value of the new node.
+ * Return: The address of the new node, or NULL if it failed.
+ * Author: Amira
+ */
 
 dlistint_t *insert_dnodeint_at_index(
 	dlistint_t **head,
@@ -17,13 +17,10 @@ dlistint_t *insert_dnodeint_at_index(
 	int n
 )
 {
-	dlistint_t *new_node;
-	dlistint_t *current_head;
-	unsigned int current_index;
+	dlistint_t *new_node, *current_head;
 
 	new_node = NULL;
 	current_head = *head;
-	current_index = 0;
 
 	if (index == 0)
 	{
@@ -31,15 +28,14 @@ dlistint_t *insert_dnodeint_at_index(
 	}
 	else
 	{
-		while (current_head != NULL && current_index < index - 1)
+		while (current_head != NULL && index-- > 1)
 		{
 			current_head = current_head->next;
-			current_index++;
 		}
 
 		if (current_head == NULL)
 		{
-			return NULL;
+			return (NULL);
 		}
 
 		new_node = malloc(sizeof(dlistint_t));
@@ -58,5 +54,5 @@ dlistint_t *insert_dnodeint_at_index(
 		}
 	}
 
-	return new_node;
+	return (new_node);
 }
