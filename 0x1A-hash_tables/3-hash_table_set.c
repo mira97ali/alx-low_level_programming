f231b6d0320e11d0f715f47886eef18bf94ff458
Mira Ali <benamara97amira@gmail.com>
2023-12-21 20:20:29 +0300

    implement hash_table_set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,92 @@
+#include "hash_tables.h"
+
+/**
+ * create_node - Creates a new hash node
+ * @key: The key
+ * @value: The value associated with the key
+ *
+ * Return: A pointer to the new node, or NULL on failure
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * update_node - Updates the value of an existing node
+ * @node: The existing node
+ * @value: The new value to set
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int update_node(hash_node_t *node, const char *value)
+{
+	char *new_value = strdup(value);
+
+	if (new_value == NULL)
+		return (0);
+
+	free(node->value);
+	node->value = new_value;
+	return (1);
+}
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: The hash table to add or update the key/value to
+ * @key: The key (cannot be an empty string)
+ * @value: The value associated with the key
+ *         (duplicated, can be an empty string)
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, adds the new node at the beginning of the list
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *temp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (update_node(temp, value));
+
+		temp = temp->next;
+	}
+
+	new_node = create_node(key, value);
+	if (new_node == NULL)
+		return (0);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
